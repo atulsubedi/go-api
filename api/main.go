d8458dd1b78a723a678703f5f58e1119ab30f903
Atul Subedi <atul.work15@gmail.com>
2023-04-15 13:25:03 +0545

    api works

--- api/main.go
+++ api/main.go
@@ -2,6 +2,30 @@ package main
 
 import(
 	"net/http"
-	"errors"
-	""
+	// "errors"
+	"github.com/gin-gonic/gin"
 )
+
+type book struct{
+	ID  	 string `json:"id"`
+	Author   string `json:"author"`
+	Title    string `json:"title"`
+	Quantity string `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Author: "Chris Guillebeau", Title:"The 100$ startup", Quantity:"8"},
+	{ID: "2", Author: "Atul Subedi", Title:"The nonsense shit", Quantity: "9"},
+	{ID: "3", Author: "Atulya Subedi", Title:"The art of moving", Quantity: "4" },
+}
+
+func getBooks(c *gin.Context){
+	c.IndentedJSON(http.StatusOK, books)
+
+}
+
+func main(){
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
+}
